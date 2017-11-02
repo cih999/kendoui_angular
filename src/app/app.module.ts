46bd5b19cda4b8e2fd290993a703a24b452d307f
cih9999@naver.com <cih9999@naver.com>
2017-11-03 00:14:37 +0900

    button module, example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
